978ec0913e3c657b55e9c2938940a810a215d5cc
Stephen Reid <stephen.reid@blinqmedia.com>
2015-03-24 03:22:53 -0400

    Add an example

---
+++ example/app.js
@@ -0,0 +1,10 @@
+/*global angular */
+'use strict';
+
+/**
+ * The main app module
+ * @name app
+ * @type {angular.Module}
+ */
+var app = angular.module('app', [])
+.controller('ValidatorsController', function($scope){});
